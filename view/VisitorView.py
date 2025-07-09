297a6e66863dea0295def9306681a13e18ff4053
Carol <caroldazevedo52@gmail.com>
2025-07-09 10:54:52 -0300

    Update VisitorView.py

---
+++ view/VisitorView.py
@@ -0,0 +1,12 @@
+from src.model.Entidades.Cliente import Cliente
+from src.model.Persistencia.Clientes import Clientes
+from src.model.Entidades.Entregador import Entregador
+from src.model.Persistencia.Entregadores import Entregadores
+
+def abrir_conta_cliente(nome, email, senha, fone):
+    c = Cliente(0, nome, email, senha, fone)
+    Clientes.inserir(c)
+
+def abrir_conta_entregador(nome, email, senha, fone):
+    c = Entregador(0, nome, email, senha, fone)
+    Entregadores.inserir(c)
