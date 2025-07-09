f943ee0981ab975bdd32e378291c1c25a60c3350
Carol <caroldazevedo52@gmail.com>
2025-07-09 10:43:37 -0300

    Create Entregador.py

---
+++ src/model/Entidades/Entregador.py
@@ -0,0 +1,27 @@
+class Entregador:
+    def __init__(self, id, nome, email,senha, fone):
+        self.__id = id
+        self.set_nome(nome)
+        self.set_email(email)
+        self.set_senha(senha)
+        self.set_fone(fone)
+    def set_id(self, id): self.__id = id
+    def set_nome(self, nome):
+        if nome == "": raise ValueError("Informe seu nome")
+        self.__nome = nome
+    def set_email(self, email):
+        self.__email = email
+    def set_senha(self,s):
+        if len(s) < 4 : raise ValueError(f"Senha deve ter no mínimo 8 caracteres")
+        self.__senha = s
+    def set_fone(self, fone): self.__fone = fone
+
+    def get_id(self): return self.__id
+    def get_nome(self): return self.__nome
+    def get_email(self): return self.__email
+    def get_senha(self): return self.__senha
+    def get_fone(self): return self.__fone
+    def __str__(self):
+        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__senha} - {self.__fone}"
+    def to_dict(self):
+        return {"id": self.__id,"nome": self.__nome,"email": self.__email, "senha": self.__senha, "fone": self.__fone}
