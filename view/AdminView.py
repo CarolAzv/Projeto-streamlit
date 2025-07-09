fdb9620813c135a216457ae9c19d88be07798da2
Carol <caroldazevedo52@gmail.com>
2025-07-09 11:04:07 -0300

    Update AdminView.py

--- view/AdminView.py
+++ view/AdminView.py
@@ -8,110 +8,112 @@ from src.model.Persistencia.Entregadores import Entregadores
 from src.model.Entidades.Produto import Produto
 from src.model.Persistencia.Produtos import Produtos
 
-def inserir_catogita(descricao):
-    c = Categoria(0, descricao)
-    Categorias.inserir(c)
+class AdminView:
 
-def inserir_catogitaUI():
-    st.title("Criar categoria")
-    with st.form("inserir_categoria_form"):
-        descrição = st.text_input("Descrição:")
+    def inserir_catogita(descricao):
+        c = Categoria(0, descricao)
+        Categorias.inserir(c)
 
-        submitted = st.form_submit_button("Finalizar")
+    def inserir_catogitaUI():
+        st.title("Criar categoria")
+        with st.form("inserir_categoria_form"):
+            descrição = st.text_input("Descrição:")
 
-    if submitted:
-        if not descrição:
-            st.error("Preencha o campo.")
-            return
-    try:   
-        inserir_catogita(descrição)
+            submitted = st.form_submit_button("Finalizar")
 
-
-def inserir_cliente(nome, email, senha, fone):
-    c = Cliente(0, nome, email, senha, fone)
-    Clientes.inserir(c)
-
-def inserir_clienteUI():
-    st.title("Criar cliente")
-    with st.form("inserir_categoria_form"):
-        nome = st.text_input("nome:")
-        email = st.text_input("email:")
-        senha = st.text_input("senha:")
-        fone = st.text_input("fone:")
-
-        submitted = st.form_submit_button("Finalizar")
-
-    if submitted:
-        if not nome or email or senha or fone:
-            st.error("Preencha todos os campos.")
-            return
-    try:   
-        inserir_cliente(nome, email, senha, fone)
+        if submitted:
+            if not descrição:
+                st.error("Preencha o campo.")
+                return
+        try:   
+            AdminView.inserir_catogita(descrição)
 
 
-def inserir_entregador(nome, email, senha, fone):
-    e = Entregador(0, nome, email, senha, fone)
-    Entregadores.inserir(e)
+    def inserir_cliente(nome, email, senha, fone):
+        c = Cliente(0, nome, email, senha, fone)
+        Clientes.inserir(c)
 
-def inserir_entregadorUI():
-    st.title("Criar entregador")
-    with st.form("inserir_categoria_form"):
-        nome = st.text_input("nome:")
-        email = st.text_input("email:")
-        senha = st.text_input("senha:")
-        fone = st.text_input("fone:")
+    def inserir_clienteUI():
+        st.title("Criar cliente")
+        with st.form("inserir_categoria_form"):
+            nome = st.text_input("nome:")
+            email = st.text_input("email:")
+            senha = st.text_input("senha:")
+            fone = st.text_input("fone:")
 
-        submitted = st.form_submit_button("Finalizar")
+            submitted = st.form_submit_button("Finalizar")
 
-    if submitted:
-        if not nome or email or senha or fone:
-            st.error("Preencha todos os campos.")
-            return
-    try:   
-        inserir_cliente(nome, email, senha, fone)
-
-
-def inserir_produto(descricao, preco, estoque):
-    p = Produto(0, descricao, preco, estoque)
-    Produtos.inserir(p)
-
-def inserir_produtoUI():
-    st.title("Criar Produto")
-    with st.form("inserir_categoria_form"):
-        descricao = st.text_input("descrição:")
-        preco = st.text_input("preço:")
-        estoque = st.text_input("estoque:")
-
-        submitted = st.form_submit_button("Finalizar")
-
-    if submitted:
-        if not id or descricao or preco or estoque:
-            st.error("Preencha todos os campos.")
-            return
-    try:   
-        inserir_produto(descricao, preco, estoque)
+        if submitted:
+            if not nome or email or senha or fone:
+                st.error("Preencha todos os campos.")
+                return
+        try:   
+            AdminView.inserir_cliente(nome, email, senha, fone)
 
 
-def atualizar_preço(id, descricao, preco, estoque):
-    c = Produto(id, descricao, preco, estoque)
-    Produtos.atualizar(c)
+    def inserir_entregador(nome, email, senha, fone):
+        e = Entregador(0, nome, email, senha, fone)
+        Entregadores.inserir(e)
 
-def atualizar_preçoUI():
-    st.title("Atualiar Produto")
-    with st.form("inserir_categoria_form"):
-        id = st.text_input("id do produto a ser atualizado:")
-        descricao = st.text_input("nova descrição:")
-        preco = st.text_input("novo preço:")
-        estoque = st.text_input("novo estoque:")
+    def inserir_entregadorUI():
+        st.title("Criar entregador")
+        with st.form("inserir_categoria_form"):
+            nome = st.text_input("nome:")
+            email = st.text_input("email:")
+            senha = st.text_input("senha:")
+            fone = st.text_input("fone:")
 
-        submitted = st.form_submit_button("Finalizar")
+            submitted = st.form_submit_button("Finalizar")
 
-    if submitted:
-        if not id or descricao or preco or estoque:
-            st.error("Preencha todos os campos.")
-            return
-    try:   
-        atualizar_preço(id, descricao, preco, estoque)
+        if submitted:
+            if not nome or email or senha or fone:
+                st.error("Preencha todos os campos.")
+                return
+        try:   
+            AdminView.inserir_cliente(nome, email, senha, fone)
 
 
+    def inserir_produto(descricao, preco, estoque):
+        p = Produto(0, descricao, preco, estoque)
+        Produtos.inserir(p)
+
+    def inserir_produtoUI():
+        st.title("Criar Produto")
+        with st.form("inserir_categoria_form"):
+            descricao = st.text_input("descrição:")
+            preco = st.text_input("preço:")
+            estoque = st.text_input("estoque:")
+
+            submitted = st.form_submit_button("Finalizar")
+
+        if submitted:
+            if not id or descricao or preco or estoque:
+                st.error("Preencha todos os campos.")
+                return
+        try:   
+            AdminView.inserir_produto(descricao, preco, estoque)
+
+
+    def atualizar_preço(id, descricao, preco, estoque):
+        c = Produto(id, descricao, preco, estoque)
+        Produtos.atualizar(c)
+
+    def atualizar_preçoUI():
+        st.title("Atualiar Produto")
+        with st.form("inserir_categoria_form"):
+            id = st.text_input("id do produto a ser atualizado:")
+            descricao = st.text_input("nova descrição:")
+            preco = st.text_input("novo preço:")
+            estoque = st.text_input("novo estoque:")
+
+            submitted = st.form_submit_button("Finalizar")
+
+        if submitted:
+            if not id or descricao or preco or estoque:
+                st.error("Preencha todos os campos.")
+                return
+        try:   
+            AdminView.atualizar_preço(id, descricao, preco, estoque)
+
+            
 #def listar_produtoUI():
