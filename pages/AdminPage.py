00e350b72139cf84958962159a989a6edbe86809
Carol <caroldazevedo52@gmail.com>
2025-07-08 23:30:57 -0300

    Update AdminPage.py

--- pages/AdminPage.py
+++ pages/AdminPage.py
@@ -1,1 +1,35 @@
 import streamlit as st
+
+def adminPage():
+    st.title("Mercado eletrônico")
+    st.header("Bem-vindo(a) Visitante")
+
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+    col5, col6, col7 = st.columns(3)
+    #col5, col6 = st.columns(2)
+    #col7 = st.columns(1)
+
+    with col1:
+        if st.button("Cadastrar categorias"):
+            st.switch_page("Categorias")
+    with col2:
+        if st.button("Cadastrar clientes"):
+           st.switch_page("Clientes")
+    with col3:
+        if st.button("Cadastrar entregadores"):
+            st.switch_page("Entregadores")
+    with col4:
+        if st.button("Cadastrar produtos"):
+           st.switch_page("Produtos")
+    with col5:
+        if st.button("Reajustar preço de produtos"):
+            st.switch_page("Reajustar")
+    with col6:
+        if st.button("Listar as compras"):
+           st.switch_page("Listar")
+    with col7:
+        if st.button("Iniciar a entrega"):
+            st.switch_page("IniciarEntregas")
+
+adminPage()
